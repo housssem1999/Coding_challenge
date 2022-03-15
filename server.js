a27f101654ddb5c1f5f7bae96fde64cf673d0dcf
housssem1999 <62448561+housssem1999@users.noreply.github.com>
2022-03-15 11:16:48 +0100

    connect to your database

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const cors = require("cors")
 var bodyParser = require('body-parser')
+const mysql = require('mysql')
+require('dotenv').config({path: './.env'})
+
+//connect to your data base
+const db = mysql.createConnection({
+    host:'localhost',
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+  })
+db.connect(()=>{
+   console.log(`connect to your database ${process.env.DATABASE}`)
+})
 
 //configure the cors
 const corsOptions ={
